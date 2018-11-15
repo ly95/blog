4e415c92a153455f102e69f1ee3bcf0c590bd45e
Lin Yang <linyang95@aol.com>
2018-11-15 17:56:03 +0800

    u

---
+++ .circleci/deploy-ghpages.sh
@@ -0,0 +1,31 @@
+remote=$(git config remote.origin.url)
+
+mkdir gh-pages-branch
+cd gh-pages-branch
+
+git init
+
+git config user.name "$USER_NAME"
+git config user.email "$USER_EMAIL"
+
+git remote add --fetch origin "$remote"
+
+if git rev-parse --verify origin/gh-pages > /dev/null 2>&1
+then
+    git checkout gh-pages
+    git rm -rf .
+else
+    git checkout --orphan gh-pages
+fi
+
+cp -r lanyon-1.0.0/_site/* .
+
+git add -A
+
+git commit --allow-empty -m "Deploy to GitHub pages [ci skip]"
+
+git push --force origin gh-pages
+
+cd ..
+
+rm -rf gh-pages-branch
